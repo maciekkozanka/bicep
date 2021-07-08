ea0987ef970caadb6b658e80deb3ac5c93f98862
Marcin Jastrzebski <majastrz@microsoft.com>
2021-07-07 20:30:49 -0700

    queue

--- src/Bicep.LangServer/Registry/ModuleRestoreScheduler.cs
+++ src/Bicep.LangServer/Registry/ModuleRestoreScheduler.cs
@@ -1,19 +1,31 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+using Azure.Deployments.Core.Extensions;
 using Bicep.Core.Modules;
 using Bicep.Core.Registry;
+using Bicep.Core.Syntax;
+using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
+using System.Diagnostics;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Bicep.LanguageServer.Registry
 {
-    public class ModuleRestoreScheduler
+    public sealed class ModuleRestoreScheduler
     {
         private readonly IModuleRegistryDispatcher dispatcher;
 
-        private readonly Queue<ImmutableArray<ModuleReference>> queue = new();
+        private readonly Queue<ImmutableArray<ModuleDeclarationSyntax>> queue = new();
+
+        private readonly CancellationTokenSource cancellationTokenSource = new();
+
+        // block on initial wait until signaled
+        private readonly ManualResetEventSlim manualResetEvent = new(false);
+
+        private Task? consumerTask;
 
         public ModuleRestoreScheduler(IModuleRegistryDispatcher dispatcher)
         {
@@ -25,18 +37,51 @@ namespace Bicep.LanguageServer.Registry
         /// Does not wait for the operation to complete and returns immediately.
         /// </summary>
         /// <param name="references">The module references</param>
-        public void RequestModuleRestore(IEnumerable<ModuleReference> references)
+        public void RequestModuleRestore(IEnumerable<ModuleDeclarationSyntax> references)
         {
             var immutable = references.ToImmutableArray();
             lock (this.queue)
             {
                 this.queue.Enqueue(immutable);
+
+                // notify consumer about new items
+                this.manualResetEvent.Set();
             }
         }
 
         public void Start()
         {
-            //var task = new Task()
+            this.consumerTask = Task.Factory.StartNew(this.ProcessQueueItems, this.cancellationTokenSource.Token, TaskCreationOptions.LongRunning, TaskScheduler.Default);
+        }
+
+        private void ProcessQueueItems()
+        {
+            var token = this.cancellationTokenSource.Token;
+            while (true)
+            {
+                this.manualResetEvent.Wait(token);
+
+                var items = new List<ModuleDeclarationSyntax>();
+                lock (this.queue)
+                {
+                    this.UnsafeCollectModuleReferences(items);
+                    Debug.Assert(this.queue.Count == 0, "this.queue.Count == 0");
+
+                    // queue has been consumed - next iteration should block until more items have been added
+                    this.manualResetEvent.Reset();
+                }
+
+                // TODO: What to do with the results?
+                var failures = this.dispatcher.RestoreModules(items);
+            }
+        }
+
+        private void UnsafeCollectModuleReferences(List<ModuleDeclarationSyntax> items)
+        {
+            while(this.queue.TryDequeue(out var partial))
+            {
+                items.AddRange(partial);
+            }
         }
     }
 }
